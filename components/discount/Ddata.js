649866e6cbf6b0652b1d417bb20dabce42834354
Nanidineshh <119720213+Nanidineshh@users.noreply.github.com>
2024-05-01 21:47:58 +0530

    Add files via upload

---
+++ components/discount/Ddata.js
@@ -0,0 +1,59 @@
+import pic1 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/cot.png";
+import pic2 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/poo.png";
+import pic3 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/tv.png";
+import pic4 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/sofa.png";
+import pic5 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/cotdouble.png";
+import pic6 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/table.png";
+import pic7 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/tab.png";
+import pic8 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/poojaa.png";
+import pic9 from "D:/E-commerce/sivasakthi/src/components/assets/files/discount/war.png";
+
+
+const Ddata = [
+    {
+      cover: pic1,
+      name: "Queen Cot",
+      price: "23000",
+    },
+    {
+      cover: pic2,
+      name: "Poojamandabam",
+      price: "9000",
+    },
+    {
+      cover: pic3,
+      name: "TV showcase",
+      price: "10000",
+    },
+    {
+      cover: pic4,
+      name: "Sofa come Bed",
+      price: "30000",
+    },
+    {
+      cover: pic5,
+      name: "Cot",
+      price: "25000",
+    },
+    {
+      cover: pic6,
+      name: "Study Table",
+      price: "6000",
+    },
+    {
+      cover: pic7,
+      name: "Teepoy",
+      price: "3500",
+    },
+    {
+      cover: pic8,
+      name: "Tv showcase",
+      price: "7000",
+    },
+    {
+      cover: pic9,
+      name: "Wardrobe",
+      price: "17000",
+    },
+  ]
+  export default Ddata
---
+++ components/top/Tdata.js
@@ -0,0 +1,30 @@
+import teep from 'D:/E-commerce/sivasakthi/src/components/assets/files/table/teecolla.jpg';
+import chair from 'D:/E-commerce/sivasakthi/src/components/assets/files/chairs/chaircolla.jpg';
+import tab from 'D:/E-commerce/sivasakthi/src/components/assets/files/table/tabcollage.jpg';
+import tv from 'D:/E-commerce/sivasakthi/src/components/assets/files/tvshow/tvcola.jpg';
+
+const Tdata = [
+    {
+      cover: chair,
+      para: "Chairs",
+      desc: "Crafted for Comfort",
+    },
+    {
+      cover: teep,
+      para: "Teepoy",
+      desc: "Versatile Beverage Table ",
+    },
+    {
+      cover: tab,
+      para: "Table",
+      desc: "Modern Study Essential",
+    },
+    {
+      cover: tv,
+      para: "TV showcase",
+      desc: "Stylish Screen Showcase",
+    },
+    
+  ]
+  
+  export default Tdata
